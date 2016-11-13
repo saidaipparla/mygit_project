433552230caa1a273f50ac1ab7671af629cee6bf
saidaipparla <saidarao1214@gmail.com>
2016-11-13 11:05:12 +0100

    my first commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+jQuery.noConflict();
+jQuery( document ).ready(function( $ ) {
+  // You can use the locally-scoped $ in here as an alias to jQuery.
+  // preloading effect
+	$(document).ready(function() {
+    $(window).load(function() {
+      $('#loader').fadeOut(1600);
+      $('#maincontent').show();
+    });
+  });
+	/*for background changing */
+	var images = [ "images/bg.jpg", "images/bg1.jpg"];
+	 var currentImage = 0;
+	 function changeBackground() {
+			 $( '#csoon' ).css( { backgroundImage: 'url(' + images[ ++currentImage ] + ')' } );
+			 if ( currentImage >= images.length - 1 ) {
+					 currentImage -= images.length;
+			 }
+			 $(".Greeting").hide();
+	 }
+	 setInterval( changeBackground, 2000 );
+	 //$(".Greeting").hide();
+});
