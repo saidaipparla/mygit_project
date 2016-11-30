839dee7127fd76edb9aeaede5018a27aa61f011c
saidaipparla <saidarao1214@gmail.com>
2016-11-30 11:36:24 +0100

    Update component.js

--- js/component.js
+++ js/component.js
@@ -1,190 +1,50 @@
-{
-    "description": "",
-    "methods": [
-        {
-            "name": "addTooltip",
-            "docblock": null,
-            "modifiers": [],
-            "params": [
-                {
-                    "name": "btn",
-                    "type": null
-                }
-            ],
-            "returns": null
-        }
-    ],
-    "props": {
-        "id": {
-            "type": {
-                "name": "string"
-            },
-            "required": false,
-            "description": "",
-            "defaultValue": {
-                "value": "\"mapstore-zoom\"",
-                "computed": false
-            }
-        },
-        "style": {
-            "type": {
-                "name": "object"
-            },
-            "required": false,
-            "description": ""
-        },
-        "glyphicon": {
-            "type": {
-                "name": "string"
-            },
-            "required": false,
-            "description": "",
-            "defaultValue": {
-                "value": "\"plus\"",
-                "computed": false
-            }
-        },
-        "text": {
-            "type": {
-                "name": "string"
-            },
-            "required": false,
-            "description": ""
-        },
-        "btnSize": {
-            "type": {
-                "name": "enum",
-                "value": [
-                    {
-                        "value": "'large'",
-                        "computed": false
-                    },
-                    {
-                        "value": "'small'",
-                        "computed": false
-                    },
-                    {
-                        "value": "'xsmall'",
-                        "computed": false
-                    }
-                ]
-            },
-            "required": false,
-            "description": "",
-            "defaultValue": {
-                "value": "'xsmall'",
-                "computed": false
-            }
-        },
-        "className": {
-            "type": {
-                "name": "string"
-            },
-            "required": false,
-            "description": "",
-            "defaultValue": {
-                "value": "\"square-button\"",
-                "computed": false
-            }
-        },
-        "help": {
-            "type": {
-                "name": "union",
-                "value": [
-                    {
-                        "name": "string"
-                    },
-                    {
-                        "name": "element"
-                    }
-                ]
-            },
-            "required": false,
-            "description": ""
-        },
-        "step": {
-            "type": {
-                "name": "number"
-            },
-            "required": false,
-            "description": "",
-            "defaultValue": {
-                "value": "1",
-                "computed": false
-            }
-        },
-        "currentZoom": {
-            "type": {
-                "name": "number"
-            },
-            "required": false,
-            "description": "",
-            "defaultValue": {
-                "value": "3",
-                "computed": false
-            }
-        },
-        "minZoom": {
-            "type": {
-                "name": "number"
-            },
-            "required": false,
-            "description": "",
-            "defaultValue": {
-                "value": "0",
-                "computed": false
-            }
-        },
-        "maxZoom": {
-            "type": {
-                "name": "number"
-            },
-            "required": false,
-            "description": "",
-            "defaultValue": {
-                "value": "28",
-                "computed": false
-            }
-        },
-        "onZoom": {
-            "type": {
-                "name": "func"
-            },
-            "required": false,
-            "description": "",
-            "defaultValue": {
-                "value": "() => {}",
-                "computed": false
-            }
-        },
-        "tooltip": {
-            "type": {
-                "name": "element"
-            },
-            "required": false,
-            "description": ""
-        },
-        "tooltipPlace": {
-            "type": {
-                "name": "string"
-            },
-            "required": false,
-            "description": "",
-            "defaultValue": {
-                "value": "\"left\"",
-                "computed": false
-            }
-        },
-        "bsStyle": {
-            "type": {
-                "name": "string"
-            },
-            "required": false,
-            "description": "",
-            "defaultValue": {
-                "value": "\"default\"",
-                "computed": false
-            }
-        }
-    }
-}
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="coming soon">
+    <meta name="author" content="saida">
+    <link rel="icon" href="">
+    <title> My Git Project </title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+      <!-- start preloading effect section-->
+    <div id='loader'>
+      <!--<p>Please be patient while loading</h1>-->
+    </div>
+    <!-- end preloading effect section-->
+    <div id="maincontent" style="display:none;">
+      <section id="csoon">
+          <div class="csoon-main">
+            <div class="csoon_inner animated fadeIn">
+                <h4 class="animated bounceInDown"> welcome </h4>
+                <span class="hrline"></span>
+                <h1 class="animated bounceInDown">MY Git Project</h1>
+                <h3 class="animated swing">From</h3>
+                <h1 class="animated pulse"> Saida</h1>
+                <h3 class=" Greeting animated slideInLeft">Hello :)</h3>
+            </div>
+          </div>
+        </section>
+    </div>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/app.js"></script>
+    <script src="https://use.fontawesome.com/9346dd558d.js"></script>
+</body>
+</html>
